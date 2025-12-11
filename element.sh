04e3f7bf207f5d99c12fb74911714752da237208
unshackled1999-png <unshackled1999@gmail.com>
2025-12-11 10:37:21 -0500

    feat: get atomic number

--- element.sh
+++ element.sh
@@ -7,3 +7,11 @@ PSQL="psql --username=freecodecamp --dbname=<database_name> -t --no-align -c"
   exit
  fi
 
+#get atomic number
+
+if [[ $1 =~ ^[0-9]+$ ]]; then
+  ATOMIC_NUMBER=$($PSQL "SELECT atomic_id from elements where atomic_id=$1;")
+else
+  ATOMIC_NUMBER=$($PSQL "SELECT atomic_id from elements where name='$1' or symbol='$1';")
+fi
+
