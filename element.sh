ba9926323f79b0eeb8bc406138bdb66ab52c3e0b
unshackled1999-png <unshackled1999@gmail.com>
2025-12-11 11:17:40 -0500

    fix: indentation error fix

--- element.sh
+++ element.sh
@@ -2,14 +2,15 @@
 PSQL="psql --username=freecodecamp --dbname=<database_name> -t --no-align -c"
 
 #check if input provided
- if [[ -z $1 ]] then
+if [[ -z $1 ]] 
+then
   echo  Please provide an element as an argument.
   exit
- fi
+fi
 
 #get atomic number
 
-if [[ $1 =~ ^[0-9]+$ ]]; then
+if [[ $1 =~ ^[0-9]+$ ]] then
   ATOMIC_NUMBER=$($PSQL "SELECT atomic_id from elements where atomic_id=$1;")
 else
   ATOMIC_NUMBER=$($PSQL "SELECT atomic_id from elements where name='$1' or symbol='$1';")
