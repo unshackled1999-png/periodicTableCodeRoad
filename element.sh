10a9d17de5c907a5a44b054e3d61a470caebe46d
unshackled1999-png <unshackled1999@gmail.com>
2025-12-11 11:15:11 -0500

    feat: get all the information

--- element.sh
+++ element.sh
@@ -15,3 +15,15 @@ else
   ATOMIC_NUMBER=$($PSQL "SELECT atomic_id from elements where name='$1' or symbol='$1';")
 fi
 
+#check if valid input
+if [[ -z $1 ]] then
+  echo I could not find that element in the database.
+  exit
+else
+  ATOMIC_INFORMATION=$($PSQL "SELECT e.atomic_number, e.name, e.symbol, p.atomic_mass, p.melting_point_celsius, p.boiling_point_celsius, t.type from elements as e inner join properties as p ON e.atomic_number = p.atomic_number inner join types as t on p.type_id = t.type_id where e.atomic_number = $ATOMIC_NUMBER;"
+fi
+echo "$ELEMENT_DATA" | while IFS="|" read -r ATOM_NUM NAME SYMBOL MASS MELT BOIL TYPE
+  do
+    echo "The element with atomic number $ATOM_NUM is $NAME ($SYMBOL). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MELT celsius and a boiling point of $BOIL celsius."
+  done
+fi
